ed60a0f5e4a9e3acd204f466081b11206958f19b
Valentín Barros <valentin@sanva.net>
2020-11-30 08:52:59 +0100

    Changed `FastVector2Tools.rotate` from degrees to radians.
    And added `FastVector2Tools.rotateDegrees`.

--- Sources/FastVector2Tools.hx
+++ Sources/FastVector2Tools.hx
@@ -2,18 +2,26 @@
 import kha.math.FastVector2;
 import kha.FastFloat;
 
+using Math;
+
 class FastVector2Tools {
 
-  public static function rotate(vec:FastVector2, angleDeg:FastFloat, origin:FastVector2):Void {
+  public static function rotate(vec:FastVector2, angle:FastFloat, origin:FastVector2):Void {
 
     final x = vec.x - origin.x;
     final y = vec.y - origin.y;
 
-    final cos = Math.cos(angleDeg * Math.PI / 180);
-    final sin = Math.sin(angleDeg * Math.PI / 180);
+    final cos = angle.cos();
+    final sin = angle.sin();
 
     vec.x = x * cos - y * sin + origin.x;
     vec.y = x * sin + y * cos + origin.y;
+
+  }
+
+  public inline static function rotateDegrees(vec:FastVector2, angleDegrees:FastFloat, origin:FastVector2):Void {
+
+    return FastVector2Tools.rotate(vec, angleDegrees * Math.PI / 180, origin);
 
   }
 
